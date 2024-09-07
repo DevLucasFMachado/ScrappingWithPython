f5d1baa4894ed5951888cd908ebb3fb152cc005d
Lucas Fernando Machado <devlucasfmachado@gmail.com>
2024-09-07 03:31:57 -0300

    Bug Fix Streamlit 1

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from scrape import scrape_website
+
 
 #Criou um titulo para o site
 st.title("Raspando dados com AI")
 #Cria um campo pedindo um URL para o usuário
-url = st.text_imput("Coloque o URL do Website")
+url = st.text_input("Coloque o URL do Website")
 #Cria um botão no Website
 if st.button("Obter os dados"):
     #Escreve no Website a string abaixo
     st.write("Obtenção em andamento...")
+    resultado = scrape_website(url)
+    print(resultado)
