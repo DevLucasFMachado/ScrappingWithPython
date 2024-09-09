a96838a8ab4b1fda6932510e9a49d4a3e56c5c0c
Lucas Fernando Machado <devlucasfmachado@gmail.com>
2024-09-09 16:03:54 -0300

    Cleaning the HTML
    Created new functions to clean the extracted html. They removes the tags and the scripts leaving only the textual content

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service 
 import time
-
+from bs4 import BeautifulSoup
 
 def scrape_website(website):
     print("Launching chrome brownser")
@@ -19,3 +19,23 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")
+    #remove os \n desnecessários providos pelo html
+    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+    return cleaned_content
+
